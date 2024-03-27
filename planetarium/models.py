a2b724b71c1184726e5280cf07e036fd22efaa67
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-27 15:43:28 +0200

    Changed main settings and also create urls files for apps

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self):
+        return self.seats_in_row * self.rows
+
+    def __str__(self):
+        return self.name
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    show_theme = models.ManyToManyField(ShowTheme, blank=True)
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        "AstronomyShow",
+        on_delete=models.CASCADE
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE
+    )
+    show_time = models.DateTimeField
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField()
+    user = get_user_model()
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE
+    )
